332633226a5de3d2f47bd811bec833568d9a3d70
Olivier Le Cam <olc@netify.fr>
2012-11-26 18:25:59 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '90da47b8f0939869ac9ff7bf3da6a8e31b84523e42ca344be6e543455406f41f5e0f7cdddd57bd4e2ef34c6767bcd4beabcae06a3f8c3fe51daa7e08b8ddcfe8'
